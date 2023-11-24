95b2a496d0f8995b0af5b36ee3289493904d21c7
Jakub Burda <jakubburdaofc@gmail.com>
2023-11-24 16:42:26 +0100

    Administrace: Vytvoření jsx formuláře pro odeslání registračních údajů

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,15 @@
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+    return <section>
+        <div className="">
+            <h1>Signup Now</h1>
+            <form action="">
+                <input type="text" placeholder="Enter your username"/>
+                <input type="text" placeholder="Enter your email"/>
+                <input type="password" placeholder="Enter your passoword"/>
+                <input type="submit" value="Registration"/>
+            </form>
+        </div>
+    </section>
 }
 
 export default Signup
