4f75a96037cbf35f367637287a1e60f3816dbed1
Jakub Burda <jakubburdaofc@gmail.com>
2023-11-24 17:04:47 +0100

    Registrace: vytvoření funkce odesílajicí data z formuláře na BE end point

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,12 +1,70 @@
+import { useState } from 'react'
+
 const Signup = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("")
+
+    /**
+     * SubmitedForm
+     * Function send registration data to the backend endpoint
+     */
+    const submitedForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = {
+            username: username,
+            email: email,
+            password: password
+        }
+
+        fetch('https://your-backend-api.com/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log('Registration successful');
+            } else {
+                console.error('Registration failed');
+                setError('Registration failed')
+            }
+        })
+        .catch(error => {
+            console.error('Error during registration:', error);
+            setError('Error during registration')
+        });
+        
+    }
+
     return <section>
         <div className="">
             <h1>Signup Now</h1>
-            <form action="">
-                <input type="text" placeholder="Enter your username"/>
-                <input type="text" placeholder="Enter your email"/>
-                <input type="password" placeholder="Enter your passoword"/>
-                <input type="submit" value="Registration"/>
+            {error && <p>{error}</p>}
+            <form action="" onSubmit={(e)=>submitedForm(e)}>
+                <input 
+                    type="text" 
+                    placeholder="Enter your username"
+                    onChange={(e)=>setUsername(e.target.value)}
+                />
+                <input 
+                    type="text"
+                    placeholder="Enter your email"
+                    onChange={(e)=>setEmail(e.target.value)}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Enter your passoword"
+                    onChange={(e)=>setPassword(e.target.value)}    
+                />
+                <input 
+                    type="submit"
+                    value="Registration"
+                />
             </form>
         </div>
     </section>
