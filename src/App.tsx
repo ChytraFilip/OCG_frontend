6bf0c0ab020a212d4a45df513dd1a56552b421d0
Jakub Burda <jakubburdaofc@gmail.com>
2023-11-24 15:08:07 +0100

    Vytvoření základních stránek pro hru, propojení pomocí routingu skrze react-router-dom, vytvoření shared layoutu pro stránky

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import SharedLayout from './pages/SharedLayout'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Game from './pages/Game'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import News from './pages/News'
 
 const App = () => {
   return <BrowserRouter>
     <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route path="/home" element={<Home />} />
+      <Route element={<SharedLayout />}>
+        <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/news' element={<News />} />
       </Route>
     </Routes>
   </BrowserRouter>
