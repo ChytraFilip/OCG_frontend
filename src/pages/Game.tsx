72dbf81bbd3d026d583d9ad8539c76854b27f9e7
Strosser <jan.strosser@gmail.com>
2023-12-14 22:27:35 +0100

    Arena bg

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,9 @@
+import arena from '../assets/panight.jpg'
 
 const Game = () => {
   return <>
-    <p>This is game page</p>
+    <section className="flex items-center justify-center h-screen text-white" style={{ backgroundImage: `url(${arena})`, backgroundSize: 'cover', fontFamily: 'Libre Baskerville, serif' }}>
+    </section>
   </>
 }
 
