a4231eb092f86c8575772cfebbae1ea635c5d9bb
lubos zeman <lubos.zeman@redrobot.cz>
2023-12-14 23:16:10 +0100

    Install StompJS websocket connection <3

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,50 +1,29 @@
 import { useEffect } from "react"
 import { useSessionContext } from '../components/sessionContext';
-// import { GameDto } from '../api/generated-sources/ocgClientFetch';
+import { type GameDto } from '../api/generated-sources/ocgClientFetch';
 import '../components/init'
 
 import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
 
 const Game = () => {
   const { session } = useSessionContext();
-  const socket = new SockJS(session?.socket ||'');
-  console.log(session?.socket)
-
-  const sendMessage = () => {
-    const message = 'Hello, server!';
-    const targetURL = '/send';
-
-    socket.send(JSON.stringify({
-      targetURL: targetURL,
-      message: message
-    }));
-  };
-
-  useEffect( ()=> {
-    if(session?.socket) {
-      socket.addEventListener('open', (event) => {
-        console.log('WebSocket connection opened:', event);
-      });
-      
-      socket.addEventListener('message', (event) => {
-        console.log('Received message:', event.data);
-      });
-      
-      socket.addEventListener('close', (event) => {
-        console.log('WebSocket connection closed:', event);
-      });
-
-      return () => {
-        socket.close();
-      };
-    }
-  },[session?.socket])
-
+  console.log("sessionSocket: ",session?.socket)
+  const socket = new SockJS('http://localhost:5173'+session?.socket);
+  const stompClient = Stomp.over(socket);
+  stompClient.connect({}, function (frame: string) {
+      console.log('Connected: ' + frame);
+        stompClient.subscribe(session?.subscribe, function (messages: string) {
+          console.log("Mesage:", messages);
+      })
+    })
+    
   return <>
      <div>
       <h1>Socket IO test via websocket</h1>
       <input type="text" placeholder="Enter you message.." />
-      <button type="button" onClick={()=>sendMessage()}>Send message</button>
+      <button type="button" onClick={() => stompClient.send(session?.send, {}, JSON.stringify({state: "TEST"} as GameDto))}>Send message</button>
+      <button type="button" onClick={() => stompClient.disconnect()}>Close</button>
     </div>
   </>
 }
