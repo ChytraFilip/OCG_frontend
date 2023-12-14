309d4f6ea54ee5124483b2ad2e7ea5456c48e6e6
Jakub Burda <jakubburdaofc@gmail.com>
2023-12-14 21:05:16 +0100

    Testing web socket via SockJs w Luba.

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,81 +1,44 @@
 import { useEffect } from "react"
-import { io } from 'socket.io-client';
 import { useSessionContext } from '../components/sessionContext';
-import { GameDto } from '../api/generated-sources/ocgClientFetch';
+// import { GameDto } from '../api/generated-sources/ocgClientFetch';
+import '../components/init'
+
+import SockJS from 'sockjs-client';
 
 const Game = () => {
   const { session } = useSessionContext();
-  const socket = io(session?.socket || '');
-  
-  // const [showConnection, setShowConnection] = useState(false);
-  // const [showConnectButton, setShowConnectButton] = useState(true);
-  // const [showDisconnectButton, setDisconnectButton] = useState(false);
-  // const [message, setMessage] = useState('');
-
-  console.log(session)
+  const socket = new SockJS(session?.socket ||'');
   console.log(session?.socket)
-  console.log(session?.subscribe)
-  console.log(socket?.send)
-
-  // const setConnected = (connected: boolean) => {
-  //   if(connected) {
-  //     setShowConnection(true);
-  //     // setShowConnectButton(false);
-  //   } else {
-  //     setDisconnectButton(true);
-  //   }
-  // };
-
-  // const connect = () => {
-  //   // Poslouchání události 'connect'
-  //   socket.on('connect', () => {
-  //     console.log('Connected to server');
-
-  //     // Odeslání zprávy na '/send'
-  //     socket.emit(session?.send || '', { message: 'Hello from client' });
-  //   });
-
-  //   // Poslouchání události 'message' na '/game-get/result'
-  //   socket.on(session?.subscribe || '', (data) => {
-  //     console.log('Received data from /game-get/result:', data);
-  //     // Zde můžete aktualizovat stav vaší React komponenty nebo vyvolat jinou akci
-  //   });
-  // };
-
-  // const disconnect = () => {
-  //   console.log("Odpojuji..disconnect()")
-  //   socket.disconnect();
-  // };
 
   const sendMessage = () => {
-    console.log("test")
-    socket.emit(session?.send || '',  {state:"test"} as GameDto);
-    console.log("Zpráva odeslána.")
+    const message = 'Hello, server!';
+    const targetURL = '/send';
+
+    socket.send(JSON.stringify({
+      targetURL: targetURL,
+      message: message
+    }));
   };
 
-  useEffect(() => {
-    socket.on(session?.subscribe || "", (data) => {
-      console.log("Zpráva přijata.");
-      console.log(data);
-      alert(data.message);
-    });
+  useEffect( ()=> {
+    if(session?.socket) {
+      socket.addEventListener('open', (event) => {
+        console.log('WebSocket connection opened:', event);
+      });
+      
+      socket.addEventListener('message', (event) => {
+        console.log('Received message:', event.data);
+      });
+      
+      socket.addEventListener('close', (event) => {
+        console.log('WebSocket connection closed:', event);
+      });
 
-    return () => {
-      socket.off(session?.subscribe || "");
-    };
-  }, [session?.subscribe, socket]);
-
-  // useEffect( ()=> {
-  //   console.log("Zahajuji připojování... connect()")
-  //   // connect();
-
-  //   return () => {
-  //     // disconnect();
-  //     // setConnected(false)
-  //     // setShowConnection(false);
-  //   };
-
-  // }, [])
+      return () => {
+        socket.close();
+      };
+    }
+  },[session?.socket])
 
   return <>
      <div>
